ed59c487b1ef42175d42eec244212273ca91ced9
Collin McCoy <collinmccoy17@gmail.com>
2017-08-17 12:35:37 -0700

    beginning screens

---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Components, Location } from 'expo';
+
+import {
+    Image,
+    Linking,
+    Platform,
+    ScrollView,
+    StyleSheet,
+    Text,
+    TextInput,
+    TouchableOpacity,
+    View,
+    Switch,
+    Dimensions,
+} from 'react-native';
+
+import { connect } from 'react-redux';
+import { MonoText } from '../components/StyledText';
+import { FontAwesome } from '@expo/vector-icons';
